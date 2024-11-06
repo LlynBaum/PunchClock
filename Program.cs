220b122c73c960c42ddb3b6c62bc1c58e8a9b565
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2024-11-06 10:40:35 +0100

    set up

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
-using M223PunchclockDotnet;
+using System.Security.Cryptography;
 using Microsoft.EntityFrameworkCore;
 using M223PunchclockDotnet.Service;
 using M223PunchclockDotnet.Model;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,10 +19,69 @@ builder.Services.AddDbContext<DatabaseContext>(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.ResolveConflictingActions (apiDescriptions => apiDescriptions.First());
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "JwtAuthApp API", Version = "v1" });
+
+    // Füge eine Sicherheitsdefinition für JWT (Bearer) hinzu
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme. 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      Example: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    // Lege die Sicherheitsanforderung fest
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+});
+
 builder.Services.AddScoped<EntryService>();
 builder.Services.AddScoped<TagService>();
 builder.Services.AddScoped<CategoryService>();
 builder.Services.AddScoped<TimeSummaryService>();
+
+// JWT Auth
+var publicKey = RSA.Create();
+publicKey.ImportFromPem(System.IO.File.ReadAllText("Keys/public.key"));
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new RsaSecurityKey(publicKey) // Verifizierung mit öffentlichem Schlüssel
+        };
+    });
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -44,16 +106,15 @@ using (var scope = app.Services.CreateScope())
 
     if (app.Environment.IsDevelopment())
     {
-        DbInitializer.Initialize(context);
+        //DbInitializer.Initialize(context);
     }
     
 }
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
